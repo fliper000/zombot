6b9ec768ee39a64624c5bca551127992ff5f9112
John Yani <vanuan@gmail.com>
2012-11-14 12:13:27 +0200

    stub for config

---
+++ config.py
@@ -0,0 +1,3 @@
+# this module is used to read configuration options from file
+# the long-term goal is that a config file would contain only username and password
+# the short-term goal is that it will read authkey needed for signing requests
