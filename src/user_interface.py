7ddc0f984c2454f2c132773a58b6286f423f06f1
John Yani <vanuan@gmail.com>
2013-01-24 22:47:27 +0200

    Better exception handling, annoying encoding issue

--- src/user_interface.py
+++ src/user_interface.py
@@ -1,15 +1,32 @@
+# coding=utf-8
 class UserPrompt(object):
 
     def prompt_user(self, prompt_string, choice):
+        if len(choice) == 0:
+            print "You have no  choice! Something went wrong."
         prompt_string += '\n' + "\n".join(
             [str(index + 1) + ": " + option
              for index, option in enumerate(choice)])
-        selected_index = -1
-        while not selected_index in range(len(choice)):
+        prompt_string = prompt_string.encode('utf-8')
+                                             # shouldn't be necessary
+                                             # in Python3
+                                             # http://bugs.python.org/issue7768
+        while True:
+            user_input = raw_input(prompt_string + "\n")
             try:
-                selected_index = int(raw_input(prompt_string + "\n")) - 1
-            except ValueError:
-                pass
+                selected_index = int(user_input) - 1
+                if self.is_valid(selected_index, choice):
+                    break
+                else:
+                    print user_input, "is invalid choice"
+            except ValueError, e:
+                print user_input, "is not a number"
         selected = choice[selected_index]
-        print "You selected " + selected
+        print "You selected", selected
         return selected
+
+    def is_valid(self, selected_index, choice):
+        if selected_index in range(len(choice)):
+            return True
+        else:
+            return False
