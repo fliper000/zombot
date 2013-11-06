bbec2ff91ecfad02e00a63d61e56a4c17ea74ff6
marilyn <painsides@gmail.com>
2013-11-06 23:10:09 +0200

    Travel buff

--- src/game_actors_and_handlers/travel_buff.py
+++ src/game_actors_and_handlers/travel_buff.py
@@ -10,22 +10,21 @@ logger = logging.getLogger(__name__)
 class GameTravelBuff(BaseActor):
     
     def perform_action(self):
-        self.travel_count()
-        #self.travel_count_2()
-        #self.travel_time()
-        #self.travel_time2()
-
-    def travel_time(self):
         is_there_travel_buff = False
         buff_list = self._get_game_state().get_state().buffs.list
+        o_id = False
         for buff in buff_list:
-            if buff.item == "@BUFF_TRAVEL_TICKET_TIME":
+            if buff.item == "@BUFF_TRAVEL_TICKET_TIME" or "@BUFF_TRAVEL_TICKET_TIME2":
                 time_exp = buff.expire.endDate
                 is_there_travel_buff = True
+            elif buff.item == "@BUFF_TRAVEL_TICKET_COUNT" or "@BUFF_TRAVEL_TICKET_COUNT2":
+                #time_exp = buff.expire.count
+                is_there_travel_buff = True
         if is_there_travel_buff == False or self._get_timer().has_elapsed(time_exp):
-            current_loc = self._get_game_state().get_location_id()
-            location_id = "isle_02"
-            if current_loc == location_id:
+            for item in self._get_game_state().get_state().gameObjects:
+                if item.item == "@B_VAN_ICE_CREAM":
+                    o_id = item.id
+            if o_id:
                 craft = "1"
                 exchange = self._get_item_reader().get("B_VAN_ICE_CREAM").crafts
                 for one_item in exchange:
@@ -40,124 +39,9 @@ class GameTravelBuff(BaseActor):
                         storage_sharp = item.count
                     elif item.item == books:
                         storage_books = item.count
-                for item in self._get_game_state().get_state().gameObjects:
-                    if item.item == "@B_VAN_ICE_CREAM":
-                        o_id = item.id
                 event = {"itemId":craft,"objId":o_id,"type":"item","action":"craft"}
                 self._get_events_sender().send_game_events([event])
-                logger.info(u"Проездной на 5 дней")
+                logger.info(self._get_item_reader().get("@BUFF_TRAVEL_TICKET_TIME").name)
                 buff_list.append(dict2obj({"item":"@BUFF_TRAVEL_TICKET_TIME", "expire": dict2obj({"type":"time", "time": str(int(self._get_timer()._get_current_client_time())+86400000*5)})}))
                 self._get_game_state().remove_from_storage(sharp, sharp_count)
                 self._get_game_state().remove_from_storage(books, books_count)
-
-    def travel_time2(self):
-        is_there_travel_buff = False
-        buff_list = self._get_game_state().get_state().buffs.list
-        for buff in buff_list:
-            if buff.item == "@BUFF_TRAVEL_TICKET_TIME2":
-                time_exp = buff.expire.endDate
-                is_there_travel_buff = True
-        if is_there_travel_buff == False or self._get_timer().has_elapsed(time_exp):
-            current_loc = self._get_game_state().get_location_id()
-            location_id = "isle_02"
-            if current_loc == location_id:
-                craft = "2"
-                exchange = self._get_item_reader().get("B_VAN_ICE_CREAM").crafts
-                for one_item in exchange:
-                    if one_item.id == craft:
-                        sharp = one_item.materials[0].item
-                        sharp_count = one_item.materials[0].count
-                        books = one_item.materials[1].item
-                        books_count = one_item.materials[0].count
-                storage = self._get_game_state().get_state().storageItems
-                for item in storage:
-                    if item.item == sharp:
-                        storage_sharp = item.count
-                    elif item.item == books:
-                        storage_books = item.count
-                for item in self._get_game_state().get_state().gameObjects:
-                    if item.item == "@B_VAN_ICE_CREAM":
-                        o_id = item.id
-                event = {"itemId":craft,"objId":o_id,"type":"item","action":"craft"}
-                self._get_events_sender().send_game_events([event])
-                logger.info(u"Проездной на 90 дней")
-                buff_list.append(dict2obj({"item":"@BUFF_TRAVEL_TICKET_TIME2", "expire": dict2obj({"type":"time", "time": str(int(self._get_timer()._get_current_client_time())+86400000*90)})}))
-                self._get_game_state().remove_from_storage(sharp, sharp_count)
-                self._get_game_state().remove_from_storage(books, books_count)
-
-    def travel_count(self):
-        is_there_travel_buff = False
-        buff_list = self._get_game_state().get_state().buffs.list
-        for buff in buff_list:
-            if buff.item == "@BUFF_TRAVEL_TICKET_COUNT":
-                time_exp = buff.expire.count
-                is_there_travel_buff = True
-        if is_there_travel_buff == False or time_exp < 1:
-            current_loc = self._get_game_state().get_location_id()
-            location_id = "isle_02"
-            if current_loc == location_id:
-                craft = "3"
-                exchange = self._get_item_reader().get("B_VAN_ICE_CREAM").crafts
-                for one_item in exchange:
-                    if one_item.id == craft:
-                        sharp = one_item.materials[0].item
-                        sharp_count = one_item.materials[0].count
-                        books = one_item.materials[1].item
-                        books_count = one_item.materials[0].count
-                        result = one_item.resultCount
-                        print result
-                storage = self._get_game_state().get_state().storageItems
-                for item in storage:
-                    if item.item == sharp:
-                        storage_sharp = item.count
-                    elif item.item == books:
-                        storage_books = item.count
-                for item in self._get_game_state().get_state().gameObjects:
-                    if item.item == "@B_VAN_ICE_CREAM":
-                        o_id = item.id
-                event = {"itemId":craft,"objId":o_id,"action":"craft","type":"item"}
-                self._get_events_sender().send_game_events([event])
-                logger.info(u"Жетоны на 100 поездок")
-                buff_list.append(dict2obj({"item":"@BUFF_TRAVEL_TICKET_COUNT", "expire": dict2obj({"type":"count", "count": 100})}))
-                self._get_game_state().remove_from_storage(sharp, sharp_count)
-                self._get_game_state().remove_from_storage(books, books_count)
-
-    def travel_count_2(self):
-        is_there_travel_buff = False
-        buff_list = self._get_game_state().get_state().buffs.list
-        for buff in buff_list:
-            if buff.item == "@@BUFF_TRAVEL_TICKET_COUNT2":
-                time_exp = buff.expire.count
-                is_there_travel_buff = True
-        if is_there_travel_buff == False or time_exp < 1:
-            current_loc = self._get_game_state().get_location_id()
-            location_id = "isle_02"
-            if current_loc == location_id:
-                craft = "4"
-                exchange = self._get_item_reader().get("B_VAN_ICE_CREAM").crafts
-                for one_item in exchange:
-                    if one_item.id == craft:
-                        sharp = one_item.materials[0].item
-                        sharp_count = one_item.materials[0].count
-                        books = one_item.materials[1].item
-                        books_count = one_item.materials[0].count
-                        result = one_item.resultCount
-                        print result
-                storage = self._get_game_state().get_state().storageItems
-                for item in storage:
-                    if item.item == sharp:
-                        storage_sharp = item.count
-                    elif item.item == books:
-                        storage_books = item.count
-                for item in self._get_game_state().get_state().gameObjects:
-                    if item.item == "@B_VAN_ICE_CREAM":
-                        o_id = item.id
-                event = {"itemId":craft,"objId":o_id,"action":"craft","type":"item"}
-                self._get_events_sender().send_game_events([event])
-                logger.info(u"Жетоны на 1000 поездок")
-                buff_list.append(dict2obj({"item":"@BUFF_TRAVEL_TICKET_COUNT2", "expire": dict2obj({"type":"count", "count": 1000})}))
-                self._get_game_state().remove_from_storage(sharp, sharp_count)
-                self._get_game_state().remove_from_storage(books, books_count)
-
-
-
