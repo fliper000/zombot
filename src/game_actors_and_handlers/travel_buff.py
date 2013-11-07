2bc623b63f7116453029db666e851ee872f9fc4d
marilyn <painsides@gmail.com>
2013-11-07 22:50:02 +0200

    Small changes

--- src/game_actors_and_handlers/travel_buff.py
+++ src/game_actors_and_handlers/travel_buff.py
@@ -6,6 +6,18 @@ from game_actors_and_handlers.base import BaseActor
 
 logger = logging.getLogger(__name__)
 
+#2013-11-07 03:03:35,344 Переходим на Остров Любви
+#2013-11-07 03:03:35,472 Перешли на isle_03
+#2013-11-07 03:03:36,045 Не осталось ресурсов для добычи
+#2013-11-07 03:04:06,243 Переходим на Дремучий
+#2013-11-07 03:04:06,316 Перешли на isle_wild
+#2013-11-07 03:04:06,596 Не осталось ресурсов для добычи
+#2013-11-07 03:04:36,739 Переходим на Мобильный остров
+#2013-11-07 03:04:36,806 Перешли на isle_mobile
+#2013-11-07 03:04:37,154 Не осталось ресурсов для добычи
+#2013-11-07 03:05:07,326 Переходим на Остров Гигантов
+
+
 
 class GameTravelBuff(BaseActor):
     
@@ -14,15 +26,20 @@ class GameTravelBuff(BaseActor):
         buff_list = self._get_game_state().get_state().buffs.list
         o_id = False
         for buff in buff_list:
-            if buff.item == "@BUFF_TRAVEL_TICKET_TIME" or "@BUFF_TRAVEL_TICKET_TIME2":
+            if buff.item == "@BUFF_TRAVEL_TICKET_TIME" or buff.item == "@BUFF_TRAVEL_TICKET_TIME2":
                 time_exp = buff.expire.endDate
                 is_there_travel_buff = True
-            elif buff.item == "@BUFF_TRAVEL_TICKET_COUNT" or "@BUFF_TRAVEL_TICKET_COUNT2":
+            elif buff.item == "@BUFF_TRAVEL_TICKET_COUNT" or buff.item == "@BUFF_TRAVEL_TICKET_COUNT2":
                 #time_exp = buff.expire.count
+                time_exp = self._get_timer()._get_current_client_time() + 86400000
                 is_there_travel_buff = True
         if is_there_travel_buff == False or self._get_timer().has_elapsed(time_exp):
             for item in self._get_game_state().get_state().gameObjects:
                 if item.item == "@B_VAN_ICE_CREAM":
+                    exchange = self._get_item_reader().get("B_VAN_ICE_CREAM").crafts
+                    o_id = item.id
+                elif  item.item == "@B_VAN_ICE_CREAM_CASH":
+                    exchange = self._get_item_reader().get("@B_VAN_ICE_CREAM_CASH").crafts
                     o_id = item.id
             if o_id:
                 craft = "1"
