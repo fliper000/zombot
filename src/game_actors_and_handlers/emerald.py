55b095f115946b901c2ffe786fb4dfd09ac06277
marilyn <painsides@gmail.com>
2013-11-20 04:13:52 +0200

    Emerald mill and light 5

--- src/game_actors_and_handlers/emerald.py
+++ src/game_actors_and_handlers/emerald.py
@@ -11,38 +11,36 @@ class EmeraldExchange(BaseActor):
     
     def perform_action(self):
         object_id = self.get_building_id("@B_MILL_EMERALD2")
-        crafts = self._get_item_reader().get("B_MILL_EMERALD2").crafts
-        for craft in crafts:
-            craft_id = craft.id
-            while self.craft_is_possible(object_id, craft):
-                self.make_craft(craft_id, object_id)
-                logger.info(craft.name)
-#                collections[first_coll] -= first_coll_count
-#                collections[second_coll] -= second_coll_count
-        object_id = self.get_building_id("@B_LIGHT_EMERALD2")
-        exchange = self._get_item_reader().get("B_LIGHT_EMERALD2").crafts
-        self.craft_is_possible(object_id, exchange)
-                
+        if object_id:
+            crafts = self._get_item_reader().get("B_MILL_EMERALD2").crafts
+            collections = self._get_game_state().get_state().collectionItems
+            for craft in crafts:
+                craft_id = craft.id
+                while self.craft_is_possible(craft, collections):
+                    self.make_craft(craft_id, object_id)
+                    logger.info(craft.name)
+                    for material in craft.materials:
+                       self.decrement_material(material, collections)
+                       
+    def decrement_material(self, material, collections):
+        new_count = getattr(collections, self._get_item_reader().get(material.item).id, 0) - material.count
+        setattr(collections, self._get_item_reader().get(material.item).id, new_count)
+                    
     def get_building_id(self, building_type):
             for item in self._get_game_state().get_state().gameObjects:
                 if item.item == building_type:
                     return item.id
                 
-    def craft_is_possible(self, object_id, craft):
-        collections = self._get_game_state().get_state().collectionItems
+    def craft_is_possible(self, craft, collections):
         first_coll = craft.materials[0].item
         first_coll_count = craft.materials[0].count
         second_coll = craft.materials[1].item
         second_coll_count = craft.materials[1].count
-        storage_coll1 = getattr(collections, first_coll, 0)
-        if storage_coll1:
-            pass
-        storage_coll2 = getattr(collections, second_coll, 0)
-        if storage_coll2:
-            pass
+        storage_coll1 = getattr(collections, self._get_item_reader().get(first_coll).id, 0)
+        storage_coll2 = getattr(collections, self._get_item_reader().get(second_coll).id, 0)
+        print storage_coll1
         return first_coll_count <= storage_coll1 and second_coll_count <= storage_coll2
                 
     def make_craft(self, craft_id, object_id):
                 event = {"itemId":craft_id,"objId":object_id,"type":"item","action":"craft"}
                 #self._get_events_sender().send_game_events([event])
-
