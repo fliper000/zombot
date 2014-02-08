ec32be9feee11755f6ee36ea8ad30d55bc889f3e
Cheater84 <vlada1975@inbox.ru>
2014-02-08 17:38:08 +0200

    Update digger_graves.py

--- src/game_actors_and_handlers/digger_graves.py
+++ src/game_actors_and_handlers/digger_graves.py
@@ -8,33 +8,39 @@ from game_actors_and_handlers.workers import ResourcePicker
 logger = logging.getLogger(__name__)
 
 class BagsPicker(ResourcePicker):
-    '''
-    {u'gainTime': u'1513587', u'started': True, u'gainDone': 452L, u'item': u'@SC_FISHER_GRAVE_BRAINER', u'materials': 0L, u'y': 24L, u'x': 33L, u'type': u'diggerGraveWithBrains', u'id': 2126L}
-    or
-    {u'started': True, u'gainDone': 452L, u'item': u'@SC_FISHER_GRAVE_BRAINER', u'materials': 3L, u'y': 24L, u'x': 33L, u'type': u'diggerGraveWithBrains', u'id': 2126L}
-    '''
 
     def get_worker_types(self):
         return [GameDiggerGrave.type, GameDiggerGraveWithBrains.type]
     
     def perform_action(self):
-        graves = self._get_game_location().get_all_objects_by_types(
-                            self.get_worker_types())
+        graves = self._get_game_location().get_all_objects_by_types(self.get_worker_types())
         for grave in graves:
+
+            # Если рыбак не работает
+            if not grave.started:
+
+                # Получаем его название
+                grave_name = self._get_item_reader().get(grave.item).name
+                
+                # Выполняем запрос на сервер
+                logger.info(u'Выгоняем работать %s %i' % (grave_name, grave.id))
+                grave_start_event = {u'type': u'item', u'action': u'start', u'objId': grave.id}
+                self._get_events_sender().send_game_events([grave_start_event])
+
+                # Исправляем game_state, чтобы при следующем цикле он не посчитал рыбака не рабочим
+                grave.started = True
+
+            # Если есть мешки у рыбака
             if grave.materials:
                 logger.info("Собираем мешки " + str(grave.materials) + ' шт.')
             for _ in range(grave.materials):
                 self._pick_material(grave, None)
                 grave.materials -= 1
-
+                
 
 class TimeGainEventHandler(object):
-    '''
-    {"type":"timeGain","action":"start","objId":23196,"gainDone":452,"gainTime":"1867126"}
-    '''
 
-    def __init__(self, item_reader, game_location,
-                  timer):
+    def __init__(self, item_reader, game_location, timer):
         self.__item_reader = item_reader
         self.__game_location = game_location
         self.__timer = timer
@@ -46,14 +52,18 @@ class TimeGainEventHandler(object):
         gameObject = self.__game_location.get_object_by_id(
             event_to_handle.objId
         )
-        worker = self.__item_reader.get(gameObject.item).name
-        if hasattr(gameObject, 'gainTime') and gameObject.gainTime and self._get_timer().has_elapsed(gameObject.gainTime):
-                logger.info(worker + u' принёс')
-                gameObject.materials += 1
-                gameObject.gainTime = None
-        if event_to_handle.action == 'start':
-            gameObject.started = True
-            gameObject.gainTime = event_to_handle.gainTime
-            logger.info(worker + u' принесёт через ' + str((int(gameObject.gainTime) - self._get_timer()._get_current_client_time())/1000/60) + u' мин.')
+        if gameObject is None:
+            logger.critical("OMG! No such object")
+            return
         else:
-            gameObject.started = False
+            worker = self.__item_reader.get(gameObject.item).name
+            if hasattr(gameObject, 'gainTime') and gameObject.gainTime and self._get_timer().has_elapsed(gameObject.gainTime):
+                    logger.info(worker + u' принёс')
+                    gameObject.materials += 1
+                    gameObject.gainTime = None
+            if event_to_handle.action == 'start':
+                gameObject.started = True
+                gameObject.gainTime = event_to_handle.gainTime
+                logger.info(worker + u' принесёт через ' + str((int(gameObject.gainTime) - self._get_timer()._get_current_client_time())/1000/60) + u' мин.')
+            else:
+                gameObject.started = False
